ba4a8aba1f36a96df7b865dc706d1b83cb998034
Carlos Cortés Sánchez <ccortes@gmv.es>
2017-06-27 17:17:53 +0200

    Commiting project files

--- FIRQuickstart/ViewController.swift
+++ FIRQuickstart/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    @IBOutlet weak var sunnyButton: UIButton!
+    @IBOutlet weak var foggyButton: UIButton!
+    
+    let conditionRef = Database.database().reference().child("condition")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        conditionRef.observe(.value) { (snap: DataSnapshot) in
+            self.conditionLabel.text = snap.value as? String
+        }
+    }
+    @IBAction func sunnyDidTouch(_ sender: Any) {
+        conditionRef.setValue(sunnyButton.titleLabel?.text)
+    }
+    @IBAction func foggyDidTouch(_ sender: Any) {
+        conditionRef.setValue(foggyButton.titleLabel?.text)
+    }
 
+    /**
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    **/
 
 }
 
